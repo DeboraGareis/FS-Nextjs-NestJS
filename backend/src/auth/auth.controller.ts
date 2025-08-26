521e47f7e3a3d77fc357725cc47c014dd99bac52
Emily Morales <15ruth05@gmail.com>
2025-08-26 16:17:55 -0300

    feat:metodo /me nuevo en el back para decodificar el token para el front, conexion con enviar mensaje con el back, creacion de useState con id del login del cliente

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,16 +1,27 @@
-import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { ApiBody, ApiTags } from '@nestjs/swagger';
+import { Request, Response } from 'express';
 
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
-  
+
   @Post('login')
   @ApiBody({ type: LoginDto })
-  signIn(@Body() signInDto: LoginDto) {
-    return this.authService.signIn(signInDto.email, signInDto.password);
+  signIn(
+    @Res({ passthrough: true }) res: Response,
+    @Body() signInDto: LoginDto,
+  ) {
+    return this.authService.signIn(signInDto.email, signInDto.password, res);
+  }
+
+  @Get('/me')
+  async datosUtiles(@Req() req: Request) {
+    console.log('✡ req: ', req, '_typodato: ', typeof req, '_');
+
+    return await this.authService.datosUtiles(req);
   }
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,10 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  //cookies con express
+  app.use(cookieParser());
+  //corss
+  const corsOptions: CorsOptions = {
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  };
+  app.enableCors(corsOptions);
   // Configuración del Swagger
   const config = new DocumentBuilder()
     .setTitle('API de Ecommerce')
