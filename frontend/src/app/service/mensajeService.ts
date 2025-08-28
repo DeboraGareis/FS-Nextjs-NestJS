0a52827d2f6c538556002b26756cd9551101a6b7
Emily Morales <15ruth05@gmail.com>
2025-08-28 18:38:37 -0300

    fix:mejore el manejo de errores y algunas anumaciones para el componente de product

--- frontend/src/app/service/mensajeService.ts
+++ frontend/src/app/service/mensajeService.ts
@@ -1,39 +1,47 @@
+import axios from "axios";
 import host from "./api";
 import { z } from "zod";
 
-//* idAdministrador es el del vendedor=>idReceptor(back)
-//* idComprador es el de la sesion iniciada=>idEmisor(back)
-//* contenido del mensaje texto(back)
-//* leido(back)debe enviar 'no'
-
 // Esquema Zod en lugar de type o interface
-const MensajeDeAyudaZod = z.object({
-  idAdministrador: z.string(),
-  idComprador: z.string(),
-  texto: z.string(),
-  leido: z.string(),
-});
+const MensajeDeAyudaZod = z
+  .object({
+    idAdministrador: z.uuid(),
+    idComprador: z.uuid(),
+    texto: z.string().min(2),
+    leido: z.string(),
+  })
+  .transform((dato) => ({
+    idEmisor: dato.idAdministrador,
+    idReceptor: dato.idComprador,
+    texto: dato.texto,
+    leido: dato.leido,
+  }));
 // Inferimos el tipo de TS a partir del esquema
 export type MensajeDeAyuda = z.infer<typeof MensajeDeAyudaZod>;
+
+// Inferimos el tipo de TS a partir del esquema que solicita el backend
+export type MensajeDeAyudaBack = z.output<typeof MensajeDeAyudaZod>;
 
 export const mensajeDeAyuda = async (dato: MensajeDeAyuda) => {
   try {
     // Validación runtime
-    const datosValidados = MensajeDeAyudaZod.parse(dato);
-    console.log("🗒 datosValidados:", datosValidados);
-    const mensajeDeAyudaBack = {
-      idEmisor: datosValidados.idAdministrador,
-      idReceptor: datosValidados.idComprador,
-      texto: datosValidados.texto,
-      leido: datosValidados.leido,
-    };
-    const res = await host.post("mensaje", mensajeDeAyudaBack);
-    console.log("🗒 datosValidados:", res);
-
+    const payload = MensajeDeAyudaZod.parse(dato);
+    const res = await host.post("mensaje", payload);
     return res.data;
   } catch (error) {
-    console.log("error(revisa mensaje de ayuda!): ", error);
-
-    throw new Error("revisas mensaje de ayuda!");
+    // tirar error de Zod
+    if (error instanceof z.ZodError) {
+      console.error("ZOD: error de validacion: ", error.errors);
+      throw new Error("Datos invalidos en mensaje de ayuda");
+    }
+    // tirar error de axios
+    if (axios.isAxiosError(error)) {
+      console.error("Error HTTP:", error.response?.data);
+      throw new Error(
+        error.response?.data?.detail ?? "Error desconocido del backend",
+      );
+    }
+    console.log("throw error: ", error);
+    throw new Error("Error inesperado al enviar mensaje de ayuda");
   }
 };
--- frontend/src/components/Product.tsx
+++ frontend/src/components/Product.tsx
@@ -1,4 +1,5 @@
 import { MensajeDeAyuda, mensajeDeAyuda } from "@/app/service/mensajeService";
+import { useAuth } from "@/context/AuthContext";
 import { useUser } from "@/context/UserContext";
 import Image from "next/image";
 import { useState } from "react";
@@ -16,9 +17,10 @@ type Props = {
 };
 
 export default function Product({ producto }: Props) {
-  const { user, setUser } = useUser();
+  //state del metodo /me del backend// quiero manejar mejor la sesion del usuario, para que se mantenga, porque cuando hay un cambio se corta, me muestra vistas que no deberia
+  const { user } = useUser();
 
-  const [mensaje, useMensaje] = useState<MensajeDeAyuda>({
+  const [mensaje] = useState<MensajeDeAyuda>({
     idAdministrador: "",
     idComprador: "",
     texto: "",
@@ -26,30 +28,30 @@ export default function Product({ producto }: Props) {
   });
 
   const handleClick = async (event) => {
-    //? queres enviar el mensaje???? 1 click se envia el mensaje
-    //left-1/2 -translate-x-1/2 sm:left-auto sm:translate-x-0 sm:right-0 mt-2 p-4 rounded-lg w-[90vw] md:w-[250px] animate-fade-in z-50 bg-white dark:bg-gray-900 shadow-lg dark:shadow-gray-900
-    mensaje.idAdministrador = user;
-    mensaje.idComprador = "9ed98a27-daa5-484e-9244-02f7c66b51a4";
-    mensaje.texto = `hola soy ${user}, quiero hablar con vos por el producto ${producto.Nombre}`;
-    mensaje.leido = "No";
-    console.log("💌 mensaje: ", mensaje);
+    try {
+      alert(`Hola, quiero saber mas del producto ${producto.Nombre}`);
+      mensaje.idAdministrador = user;
+      mensaje.idComprador = "9ed98a27-daa5-484e-9244-02f7c66b51a4";
+      mensaje.texto = `Hola, quiero saber mas del producto ${producto.Nombre}`;
+      mensaje.leido = "No";
 
-    const res = await mensajeDeAyuda(mensaje);
-    console.log("res: ", res);
-
-    alert("mensaje enviado");
+      const res = await mensajeDeAyuda(mensaje);
+      alert("mensaje enviado");
+    } catch (error) {
+      console.error("Error en handleClick:", error);
+      alert(error.message);
+    }
   };
 
   return (
-    //<div className="flex flex-col items-center border rounded-lg p-4 w-48 shadow-md">
-    <div className="flex flex-col items-center border border-gray-300 p-2 w-48 shadow-md">
+    <div className="flex flex-col items-center border-gray-300 p-2 w-48 shadow-md">
       {/* Precio */}
       <p className="text-lg font-semibold text-emerald-600 mb-2">
         ${producto.Precio}
       </p>
 
       {/* Imagen optimizada */}
-      <div className="relative w-32 h-32 mb-2">
+      <div className="group relative hover:w-64 hover:h-64 transition-all duration-300 w-32 h-32 mb-2">
         <Image
           src={producto.Imagen}
           alt={producto.Nombre}
@@ -59,18 +61,21 @@ export default function Product({ producto }: Props) {
       </div>
 
       {/* Botón de ayuda + cantidad */}
-      <div className="flex items-center gap-2 group">
+      <div className="relative group flex items-center gap-2">
         <button
           role="img"
           aria-label="info"
           className="text-xl bg-gray-200 rounded-full w-7 h-7 flex items-center justify-center hover:bg-gray-300 motion-safe:hover:scale-120"
-          onDoubleClick={handleClick}
+          onClick={handleClick}
         >
           ?
-        </button>{" "}
-        <span className="absolute left-1/3 -translate-x-1.2 mt-2 px-2 py-1 text-sm text-1E4137 bg-145B46 rounded opacity-0 group-hover:opacity-100 transition-opacity">
-          {" "}
-          Consulta por el producto con el vendedor
+        </button>
+
+        <span
+          onClick={handleClick}
+          className="absolute block -translate-x-1.2 mt-28 p-2 text-xs text-1E4137 bg-CFFBEE rounded opacity-0 group-hover:opacity-100 transition-opacity"
+        >
+          Click para comunicarse con el vendedor por {producto.Nombre}
         </span>
         <p className="text-sm text-gray-700">Stock: {producto.Stock}</p>
       </div>
