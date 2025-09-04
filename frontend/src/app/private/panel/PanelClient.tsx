6214598aa2a221d7428ba77f0568797b5d27f7e9
DeboraGareis <deboragareis1@gmail.com>
2025-09-03 22:06:43 -0300

    feat: permitir crear producto solo a vendedor, buscador de productos

--- frontend/src/app/private/panel/PanelClient.tsx
+++ frontend/src/app/private/panel/PanelClient.tsx
@@ -5,6 +5,16 @@ import Product, { ProductType } from "@/components/Product";
 import Seller, { SellerType } from "@/components/Seller";
 import Search from "@/components/Search";
 import { useUser } from "@/context/UserContext";
+import { useEffect, useState } from "react";
+import { ObtenerUserPorId } from "@/app/service/userService";
+
+export type UserType = {
+  id: string;
+  nombre: string;
+  password: string;
+  email: string;
+  activo: boolean;
+};
 
 type Props = {
   productos: ProductType[];
@@ -13,27 +23,50 @@ type Props = {
 
 export default function PanelClient({ productos, vendedores }: Props) {
   const { user } = useUser();
-  console.log("user: ", user);
+  const [rtaUser, setRtaUser] = useState<UserType>({
+    id: "",
+    nombre: "",
+    password: "",
+    email: "",
+    activo: false,
+  });
+   const [productosFiltrados, setProductosFiltrados] = useState<ProductType[]>(productos);
+  useEffect(() => {
+    const DataUser = async () => {
+      if (user) {
+        const data = await ObtenerUserPorId(user);
+        if (data) setRtaUser(data);
+      }
+    };
+    DataUser();
+  }, [user]);
+
+  console.log("user: ", user, "//activo o no:", rtaUser.activo);
   console.log("/////productos:  ", productos, "/////vendedores: ", vendedores);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <div className="flex gap-4">
-        <Link
-          href="/private/panel/new_product"
-          className="text-emerald-600 hover:bg-emerald-100 text-2xl font-extrabold px-7 py-5"
-        >
-          + Nuevo producto
-        </Link>
-        <Link
-          href={`/private/panel/${user}`}
-          className="bg-emerald-600 hover:bg-emerald-400 text-2xl font-extrabold text-white px-7 py-5 rounded-lg transition"
-        >
-          Ver mis productos
-        </Link>
-      </div>
+    <div className="flex flex-col min-h-screen items-center justify-start p-8 pb-10 gap-10 sm:p-10">
+      
+      {/* Botones solo para vendedores */}
+      {rtaUser.activo && (
+        <div className="flex gap-4">
+          <Link
+            href="/private/panel/new_product"
+            className="text-emerald-600 hover:bg-emerald-100 text-2xl font-extrabold px-7 py-5"
+          >
+            + Nuevo producto
+          </Link>
+          <Link
+            href={`/private/panel/${user}`}
+            className="bg-emerald-600 hover:bg-emerald-400 text-2xl font-extrabold text-white px-7 py-5 rounded-lg transition"
+          >
+            Ver mis productos
+          </Link>
+        </div>
+      )}
 
       {/* lista vendedores */}
-      <div className="flex-col w-full">
+      <div className="flex-col w-full gap-4">
         <div className="flex">
           <div className="text-emerald-600 text-2xl font-extrabold my-4 px-7 py-5">
             Otras tiendas
@@ -46,13 +79,21 @@ export default function PanelClient({ productos, vendedores }: Props) {
             </div>
           </div>
 
+          {/* lista productos */}
           <div className="text-emerald-600 text-2xl font-extrabold my-4 px-7 py-5">
             <p>Otros productos</p>
-            <Search />
+            <Search
+              productos={productos}
+              onResults={(res) => setProductosFiltrados(res)}
+            />
             <div className="flex flex-wrap border rounded-lg my-4 justify-center gap-6 px-7 py-5">
-              {productos.map((p, i) => (
-                <Product key={i} producto={p} />
-              ))}
+              {productosFiltrados.length > 0 ? (
+                productosFiltrados.map((p, i) => (
+                  <Product key={i} producto={p} />
+                ))
+              ) : (
+                <p className="text-gray-500 text-sm">No se encontraron productos</p>
+              )}
             </div>
           </div>
         </div>
--- frontend/src/app/private/panel/[id_vendedor]/VendedorClient.tsx
+++ frontend/src/app/private/panel/[id_vendedor]/VendedorClient.tsx
@@ -2,26 +2,29 @@
 import BottonPanel from "@/components/BottonPanel";
 import Product, { ProductType } from "@/components/Product";
 import Search from "@/components/Search";
+import { useState } from "react";
 
 type Props = {
   productos: ProductType[];
 };
 
 export default function VendedorClient({ productos }: Props) {
+  const [productosFiltrados, setProductosFiltrados] = useState<ProductType[]>(productos);
   return (
     <div>
       <div className="text-emerald-600 text-2xl font-extrabold my-4 px-7 py-5">
         <p>Productos del vendedor</p>
-        <Search />
+        <Search productos={productos}
+              onResults={(res) => setProductosFiltrados(res)}/>
 
         <div className="flex flex-wrap border rounded-lg my-4 justify-center gap-6 px-7 py-5">
-          {productos && productos.length > 0 ? (
-            productos.map((producto: ProductType, index: number) => (
-              <Product key={index} producto={producto} />
-            ))
-          ) : (
-            <p className="text-gray-500 text-sm">No se encontraron productos para ese vendedor.</p>
-          )}
+          {productosFiltrados.length > 0 ? (
+                          productosFiltrados.map((p, i) => (
+                            <Product key={i} producto={p} />
+                          ))
+                        ) : (
+                          <p className="text-gray-500 text-sm">No se encontraron productos para el vendedor</p>
+                        )}
         </div>
       </div>
       <BottonPanel />
