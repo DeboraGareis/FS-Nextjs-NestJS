83132fd607ef3eaa3ec305601dbe227da2cc38d3
DeboraGareis <deboragareis1@gmail.com>
2025-09-19 15:21:41 -0300

    feat: sin verificacion de ESLint errors.

--- frontend/src/app/service/authService.ts
+++ frontend/src/app/service/authService.ts
@@ -37,7 +37,7 @@ export type RespuestaRegisterDelBack = z.infer<
 //coneccion con APIs
 export const signin = async (login: Login) => {
   try {
-    const res = await host.post<RespuestaSigninBack>("auth/login", login, {
+    await host.post<RespuestaSigninBack>("auth/login", login, {
       withCredentials: true,
     });
     // res.data;
