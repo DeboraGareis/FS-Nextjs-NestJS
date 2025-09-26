bc5b0b422329a04910fadf710d9122561e56d983
Emily Morales <15ruth05@gmail.com>
2025-09-26 12:32:16 -0300

    fix:cambios en la navbar

--- frontend/src/app/navbar/Navbar.tsx
+++ frontend/src/app/navbar/Navbar.tsx
@@ -1,39 +1,39 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+// import { useRouter } from "next/navigation";
+// import { useEffect, useState } from "react";
 
 import { useAuth } from "@/context/AuthContext";
-import { ClosedSession, Me } from "../service/authService";
-import { useUser } from "@/context/UserContext";
+import { ClosedSession } from "../service/authService";
+// import { useUser } from "@/context/UserContext";
 
 const Navbar = () => {
-  const router = useRouter();
-  const { setUser } = useUser();
+  // const router = useRouter();
+  // const { setUser } = useUser();
   const { sesion, setSesion } = useAuth();
-  const [isMounted, setIsMounted] = useState(false);
+  // const [isMounted, setIsMounted] = useState(false);
 
-  useEffect(() => {
-    setIsMounted(true);
+  // useEffect(() => {
+  //   setIsMounted(true);
 
-    const buscarUsuario = async () => {
-      try {
-        const res = await Me();
+  //   const buscarUsuario = async () => {
+  //     try {
+  //       const res = await Me();
 
-        if (!res) return router.push("/signin");
-        if (isMounted) {
-          router.push("/private/panel");
-          setUser(res.userId);
-          setSesion(true);
-        }
-      } catch (error) {
-        console.error(error);
-        router.push("/signin");
-      }
-    };
-    buscarUsuario();
-  }, [setSesion, setUser, isMounted, router]);
+  //       if (!res) return router.push("/signin");
+  //       if (isMounted) {
+  //         router.push("/private/panel");
+  //         setUser(res.userId);
+  //         setSesion(true);
+  //       }
+  //     } catch (error) {
+  //       console.error(error);
+  //       router.push("/signin");
+  //     }
+  //   };
+  //   buscarUsuario();
+  // }, [setSesion, setUser, isMounted, router]);
 
   return (
     <nav className="container mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 w-full h-18 bg-white shadow flex items-center justify-between">
